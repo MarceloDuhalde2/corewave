cb80252db2edf6480b822614bd5ecc9eb4e62925
Marcelo Duhalde <marcelohduhalde@gmail.com>
2025-05-15 20:03:48 -0300

    modificaciones en htaccess para que redirija a https y se argega header de meta para verificacion de dominio

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="{{ asset('css/aos.css') }}">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/fontawesome/all.min.css') }}">
+    <meta name="facebook-domain-verification" content="9yw3v58dfngveunj7er9tz29ruuptz" />
 </head>
 <body class="font-inter bg-gray-100">
     <!-- Navbar -->
